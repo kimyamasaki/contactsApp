97a6b36dbea38f41d78346d42dca8e6e299f7ec9
Tiphanie Lau <tiphanie.lau@gmail.com>
2015-02-23 03:31:57 -0800

    Added accelerated/parallax effect to scrollbar

--- app/js/app.js
+++ app/js/app.js
@@ -12,29 +12,62 @@ window.onload = function() {
 	//   preventDefault: true
 	// };
 
+
+  // $('#scrollbar').scroll(function() {
+  //   s = $('#scrollbar').scrollTop();
+  //   $('#main').css("-webkit-transform", "translateY(" + (s*10) + "px)");
+  // })
+
 	var main = new Hammer(document.getElementById("main"));
 
 	Hammer(document.getElementById("main")).on("swipe", function() {
-          console.log("hello");
-          $('#scrollbar').animate({right: "0"}, 500)  
-    });
+      console.log("hello");
+      $('#scrollbar').animate({right: "0"}, 500)  
+  });
 
 
-	var start, end;
+  // Hammer(document.getElementById("#contact-" + contact.id)).on("swipe", function() {
+  //     console.log(contact.id);
+  // });
+  
+  
+  var scrollbar = new Hammer(document.getElementById("scrollbar"));
+  var scrollVal = 0;
+  var mainHeight = $('#main').height();
+  var scrollbarHeight = $('#scrollbar').height();
+  var coeff = mainHeight / scrollbarHeight;
 
-    Hammer(document.getElementById("scrollbar")).on("panstart", function(e) {
-        // console.log("start");
-        start = e.distance
-        // $('html, body').animate({scrollTop: "0"}, 500);
-    });
+  console.log(mainHeight);
+  console.log(scrollbarHeight);
 
-    Hammer(document.getElementById("scrollbar")).on("panend", function(e) {
-        
-        end = e.distance       
-        var totalDistance = end - start;
-		console.log(totalDistance);
-        $('html, body').animate({scrollTop: ($(window).scrollTop()+1) * (totalDistance)}, 500);
-    });
+  scrollbar.get('pan').set({ direction: Hammer.DIRECTION_VERTICAL });
+
+  // listen to events...
+  scrollbar.on("panup pandown", function(ev) {
+      console.log(ev.type +" gesture detected.");
+
+      scrollVal = $('#scrollbar').scrollTop();
+      console.log(scrollVal);
+
+      $('html, body').animate({ scrollTop: scrollVal * coeff }, { duration: 10, easing: 'swing'});
+  });
 };
 
 
+$.fn.scrollTo = function( target, options, callback ){
+  if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+  var settings = $.extend({
+    scrollTarget  : target,
+    offsetTop     : 50,
+    duration      : 500,
+    easing        : 'swing'
+  }, options);
+  return this.each(function(){
+    var scrollPane = $(this);
+    var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+    var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+    scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+      if (typeof callback == 'function') { callback.call(this); }
+    });
+  });
+}
