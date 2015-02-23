3fccf835e50d65cbc7522bbf5d4165d70e04cda2
Tiphanie Lau <tiphanie.lau@gmail.com>
2015-02-22 16:05:21 -0800

    Scrollbar appears on swipe (horizontally). Some erratic behaviours on pan. Need to be able to overwrite vertical scrolling

--- app/js/app.js
+++ app/js/app.js
@@ -4,3 +4,37 @@ var contactsApp = angular.module('contactsApp', [
   'ngRoute',
   'controllers'
 ]);
+
+
+
+window.onload = function() {
+	// var options = {
+	//   preventDefault: true
+	// };
+
+	var main = new Hammer(document.getElementById("main"));
+
+	Hammer(document.getElementById("main")).on("swipe", function() {
+          console.log("hello");
+          $('#scrollbar').animate({right: "0"}, 500)  
+    });
+
+
+	var start, end;
+
+    Hammer(document.getElementById("scrollbar")).on("panstart", function(e) {
+        // console.log("start");
+        start = e.distance
+        // $('html, body').animate({scrollTop: "0"}, 500);
+    });
+
+    Hammer(document.getElementById("scrollbar")).on("panend", function(e) {
+        
+        end = e.distance       
+        var totalDistance = end - start;
+		console.log(totalDistance);
+        $('html, body').animate({scrollTop: ($(window).scrollTop()+1) * (totalDistance)}, 500);
+    });
+};
+
+
